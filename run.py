95adc00b89a88868ad14e79aaea4261650ed7d16
asu-id <121171802+asu-id@users.noreply.github.com>
2022-12-24 21:22:37 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 import masuk as yacek
-os.system('git pull')
-os.system("rm -rf results/OK/...")
-os.system("rm -rf results/CP/...")
+
+os.system('git pull');os.system("rm -rf results/OK/...");os.system("rm -rf results/CP/...")
+
 yacek()
