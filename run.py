ae9548d5def2b56f77b310fc64a33a74cdb344d1
asu-id <121171802+asu-id@users.noreply.github.com>
2022-12-25 14:55:32 +0700

    Update run.py

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ import os
 from masuk import masuk as yacek
 
 if __name__ == '__main__':
-    os.system('git pull');os.system("rm -rf results/OK/...");os.system("rm -rf results/CP/...")
+    os.system('git pull');os.remove("results/OK/...");os.remove("results/CP/...")
     yacek()
